717c49085d78e6d3fe1931008b301a1084d89487
Daniel Levy <daniell@aptivate.org>
2017-03-20 10:04:47 +0000

    Remove debugging code

--- django/website/contacts/migrations/0011_set_user_last_viewed_organization.py
+++ django/website/contacts/migrations/0011_set_user_last_viewed_organization.py
@@ -8,8 +8,7 @@ def set_default_last_viewed_organization(apps, schema_editor):
     Organization = apps.get_model('organizations', 'Organization')
     User = apps.get_model('contacts', 'User')
     organization = Organization.objects.first()
-    import rpdb2
-    rpdb2.start_embedded_debugger("abc")
+
     for user in User.objects.all():
         user._preferences.last_viewed_organization = organization
         user._preferences.save()
